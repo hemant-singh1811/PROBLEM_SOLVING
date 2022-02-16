2cd542df127646ef13e64ebb058b9ede6759555a
Hemant Singh <65394010+hemant-singh1811@users.noreply.github.com>
2022-02-16 21:33:41 +0530

    Time: 4 ms (29.65%), Space: 46.3 MB (5.80%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -2,33 +2,27 @@ class Solution {
     static List<List<Integer>> ans;
     public List<List<Integer>> permute(int[] nums) {
        ans=new ArrayList<>();
-        boolean visited[]=new boolean[21]; 
+        boolean visited[]=new boolean[nums.length]; 
         per(nums,visited, new ArrayList<>());
         return ans;
     }
     
     public static void per(int[] nums,boolean[] visited,ArrayList<Integer> arr)     {
-       if(arr.size()==nums.length){
-           // System.out.println("this is");
+       if(arr.size()==nums.length){ 
            ans.add(arr);
            return ;
        } 
         int n=nums.length;
         
         for(int i=0;i<n;i++){
-            int el=nums[i];
-            int idx=el;
+            int el=nums[i]; 
             ArrayList<Integer> subans=new ArrayList<>(arr);
-            
-            if(el<0){
-             // el=el*-1;
-                idx=10+el*-1;
-            }
-            if(!visited[idx]){ 
-                visited[idx]=true;
+             
+            if(!visited[i]){ 
+                visited[i]=true;
                 subans.add(el);
                 per(nums,visited,subans);
-                visited[idx]=false;                
+                visited[i]=false;                
             } 
         } 
     } 
