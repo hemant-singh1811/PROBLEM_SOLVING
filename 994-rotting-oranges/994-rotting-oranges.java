c968569817ed7624728eff7d485608e25a27d03f
Hemant Singh <65394010+hemant-singh1811@users.noreply.github.com>
2022-03-17 10:05:30 +0530

    Time: 4 ms (47.26%), Space: 42.6 MB (53.64%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -10,11 +10,14 @@ class pos {
 
 class Solution {
     public int orangesRotting(int[][] arr) {
-        		int n = arr.length;
+        
+        int n = arr.length;
 		int m = arr[0].length;
 
 		Queue<pos> que = new ArrayDeque<>();
+        
 		int one = 0;
+        
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				if (arr[i][j] == 2)
@@ -32,46 +35,39 @@ class Solution {
 		}
         
         if(one==0) return 0;
+        
 		int time = 0;
+        
 		Queue<pos> child = new ArrayDeque<>();
-		while (!que.isEmpty()) {
+		int[] dx={-1,0,1,0};
+        int[] dy={0,1,0,-1};
+        while (!que.isEmpty()) {
 			pos p = que.remove();
 			int row = p.i;
 			int col = p.j;
 
-			if (row - 1 >= 0 && arr[row - 1][col] == 1) {
-				arr[row - 1][col] = 2;
+            for(int i=0;i<4;i++){
+                
+                int newx=row+dx[i];
+                int newy=col+dy[i];
+                
+                if (newx>= 0 && newy>=0 && newx<n && newy<m && arr[newx][newy] == 1) {
+				arr[newx][newy] = 2;
 				one--;
-				child.add(new pos(row - 1, col));
-			}
-			if (row + 1 < n && arr[row + 1][col] == 1) {
-				arr[row + 1][col] = 2;
-				one--;
-				child.add(new pos(row + 1, col));
-			}
-			if (col - 1 >= 0 && arr[row][col - 1] == 1) {
-				arr[row][col - 1] = 2;
-				one--;
-				child.add(new pos(row, col - 1));
-			}
-			if (col + 1 < m && arr[row][col + 1] == 1) {
-				one--;
-				arr[row][col + 1] = 2;
-				child.add(new pos(row, col + 1));
-			}
-
+				child.add(new pos(newx, newy));
+			    }   
+              } 
 			if (que.size() == 0) {
-				if (child.size() != 0)
-					time++;
-				while (!child.isEmpty()) {
+				if (child.size() != 0)  time++;
+                
+				while (!child.isEmpty()) 
 					que.add(child.remove());
-				}
+				
 			}
-		
 		}
-		if(one==0)
-		return time;
-		else return -1;
+        
+		if(one==0)  return time;
+		else        return -1;
 		
     }
 }
