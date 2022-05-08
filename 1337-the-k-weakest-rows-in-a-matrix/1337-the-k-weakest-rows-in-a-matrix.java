7f0b16f49be16d8b88e64026d88d70baac74b424
Hemant Singh <65394010+hemant-singh1811@users.noreply.github.com>
2022-05-08 11:13:13 -0700

    Time: 1 ms (99.77%), Space: 48.2 MB (74.32%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,19 +1,56 @@
 class Solution {
-    public int[] kWeakestRows(int[][] mat, int k) {
-        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
-        int pos=0;
-        for (int[] row : mat){
-            int lo = 0, hi = row.length; 
-            while(lo < hi){
-                int mid = (lo + hi) / 2;
-                if (row[mid] != 0) lo = mid + 1;
-                else hi = mid;
-            }
-            q.add(new int[]{lo, pos++});
-        }
+	// Implement a basic linked list node
+    public class Node {
+        int val;
+        Node next;
         
-        int[] output = new int[k];
-        for(int i = 0; i < k; i++)  output[i] = q.remove()[1];
-        return output;
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+    
+    public int[] kWeakestRows(int[][] mat, int k) {
+        var mMax = mat.length;
+        var nMax = mat[0].length;
+
+        var toReturn = new int[k];
+        var foundCount = 0;
+		
+		// Use a dummy root node to anchor the list
+        var dummy = new Node(-1);
+        var current = dummy;
+		
+		// Create the list of rows
+        for (var i = 0; i < mMax; i++) {
+            current.next = new Node(i);
+            current = current.next;
+        }
+
+        current = dummy;
+		
+		// For each column, while the list has contents
+        for (var i = 0; i < nMax && current.next != null; i++) {
+			// Iterate through the remaining rows we haven't yet added to the results
+            while (current.next != null) {
+				// We are iterating rows in i < j order, so if we find a weak row, add it immediately. Remove it from the linked list
+                if (mat[current.next.val][i] == 0) {
+                    toReturn[foundCount++] = current.next.val;                  
+                    if (foundCount == k) return toReturn; // If we're done, return ASAP
+                    current.next = current.next.next;
+                }
+                else {
+                    current = current.next;  
+                }
+            }
+            current = dummy; // Reset the linked list pointer for the next iteration
+        }
+		
+		// If we have nodes left, the rows are all 1s. Add them until we're done.
+        while (foundCount < k) {
+            toReturn[foundCount++] = dummy.next.val;
+            dummy.next = dummy.next.next;
+        }
+
+        return toReturn;
     }
 }
