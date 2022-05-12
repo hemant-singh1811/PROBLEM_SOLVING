97c17a3ee0a2d873e090946d2e3e395bb9fea6f3
Hemant Singh <65394010+hemant-singh1811@users.noreply.github.com>
2022-05-12 11:26:18 -0700

    Time: 3 ms (24.06%), Space: 42.1 MB (93.18%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -1,34 +1,38 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
         
-        int i=1;
-        int j=nums.length;
-        int ans=0;
+     int i=1;
+     int j=nums.length;
+     int ans=0;
         
         while(i<=j){
-            int mid=(i+j)/2; 
-            if(maxwidsum(mid,nums)>=target){
+            int mid=(i+j)/2;
+            
+            if(lensum(nums,mid)>=target){
                 ans=mid;
                 j=mid-1;
-            }else{
+            }else
                 i=mid+1;
-            }
         }
         return ans;
     }
-    public static int maxwidsum(int k,int[] nums){
+    
+    public static int lensum(int[] nums,int len){
         int sum=0;
         int maxsum=0;
-        int n=nums.length;
-        for(int i=0;i<k;i++) sum+=nums[i];
+        
+        for(int i=0;i<len;i++){
+            sum+=nums[i];
+        }
         maxsum=sum;
-        for(int i=1;i<n-k+1;i++){
+        
+        for(int i=1;i<nums.length-len+1;i++){
             
             sum-=nums[i-1];
-            sum+=nums[i+k-1];
-            maxsum=Math.max(maxsum,sum);            
+            sum+=nums[i+len-1];            
+            
+            maxsum=Math.max(maxsum,sum);
         }
-        // if(k==5) System.out.println(maxsum);
-        return maxsum;
+       return maxsum;
     }
 }
