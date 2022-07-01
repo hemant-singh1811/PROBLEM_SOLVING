76cf377bdac96eb936894b79e8fdfd4af56267f4
Hemant Singh <65394010+hemant-singh1811@users.noreply.github.com>
2022-07-01 23:23:49 +0530

    Time: 8 ms (28.87%), Space: 55.2 MB (79.33%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,5 +1,57 @@
 class Solution { 
 	public static int numIslands(char[][] grid1) {
+	        
+		int r = grid1.length;
+		int c = grid1[0].length;
+
+        int[][] grid=new int[r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid1[i][j]=='1'){
+                    grid[i][j]=1;
+                }
+            }
+        } 
+		int ans = 0;
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				if (grid[i][j] == 1) {
+					ans++;
+					dfs(grid, i, j);
+				}
+			}
+		}
+		return ans;
+	}
+
+	public static void dfs(int[][] grid, int i, int j) {
+		int[] dx = { 1, 0, 0, -1 };
+		int[] dy = { 0, 1, -1, 0 };
+
+		grid[i][j] = 0;
+
+		for (int k = 0; k < 4; k++) {
+			int ni = i + dx[k];
+			int nj = j + dy[k];
+			if (limit(ni, nj, grid) && grid[ni][nj] == 1) {
+				dfs(grid, ni, nj);
+			}
+		}
+
+	}
+
+	private static boolean limit(int ni, int nj, int[][] grid) {
+		int r = grid.length;
+		int c = grid[0].length;
+
+		if (ni > -1 && nj > -1 && ni < r && nj < c)
+			return true;
+
+		return false;
+	}
+
+
+	public static int numIslands1(char[][] grid1) {
 		DSU d1 = new DSU();
 
         
